1a00f9a34b91a3a1cf14e5d8c29c3eefe179f903
ezzAll <155382315+ezzAll@users.noreply.github.com>
2024-01-01 22:02:37 +0500

    Create kb.h

---
+++ kb.h
@@ -0,0 +1,17 @@
+#ifndef KB_H
+#define KB_H
+
+//#include <Arduino.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+
+uint8_t PinRead(uint8_t port); 
+uint8_t AnalogRead(uint8_t port);
+
+void digitalPinWrite(uint8_t port,uint8_t e);
+void SetDigitalPinMode(uint8_t port, bool mode);
+void SetAnalogPinMode(uint8_t port, uint8_t mode);
+
+void digitalInterrupt(uint8_t port,void (*function)(),uint8_t mode);
+#endif
